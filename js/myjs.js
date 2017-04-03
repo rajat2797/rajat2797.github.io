5775adffbce56c1eb623002ba638caae4eafb926
rajat2797 <rajatsinghal1997@gmail.com>
2017-04-03 16:09:14 +0530

    first commit

---
+++ js/myjs.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+
+    $(function(){
+    	$(".typed").typed({
+    		strings: ["Python Developer", "Android Developer", "Competetive Programmer"],
+    		typeSpeed: 30,
+    		loop: true
+    	});
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();   
+
+});
