ee2a9467ec77ce002bdf1acbf845dbeb6faeef18
rajat2797 <rajatsinghal1997@gmail.com>
2017-05-21 11:02:54 +0530

    Updates

---
+++ pancakes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pp;
+#define pb push_back
+
+int main()
+{
+	ll t,i,n;
+	cin>>t;
+	t=1;
+	while(t--)
+	{
+		cin>>n;
+	}
+	return 0;
+}
